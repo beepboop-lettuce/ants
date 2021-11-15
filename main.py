e88c2c87364dd5bc98a26239a55252878709b252
beepboop-lettuce <ericaharris0486@gmail.com>
2021-11-14 22:06:34 -0600

    -created pydantic model to represent an Ant -defined route to create new Ant

--- main.py
+++ main.py
@@ -4,6 +4,23 @@ from typing import Optional, List
 
 app = FastAPI()
 
+class Ant(BaseModel):
+    domain: str
+    kingdom: str
+    phylum: str
+    classs: str
+    order: str
+    family: str
+    genus: str
+    species: str
+
+    class Config:
+        orm_mode = True
+
+@app.post('/ants/')
+async def create_ant_view(ant: Ant):
+    return ant
+
 
 @app.get('/')
 async def root():
