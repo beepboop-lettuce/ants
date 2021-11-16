3cd69ba726c237c7b1e7671b562404bb6fec57f6
beepboop-lettuce <ericaharris0486@gmail.com>
2021-11-15 21:58:28 -0600

    Adding sqlalchemy dependencies

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI, Depends
 from pydantic import BaseModel
 from typing import Optional, List
+from sqlalchemy import create_engine
+from sqlalchemy.orm import declarative_base, sessionmaker, Session
+from sqlalchemy import Boolean, Column, Float, String, Integer 
 
 app = FastAPI()
 
